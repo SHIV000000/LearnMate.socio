10591d31fb4e0ca8f085a65fd62e7cc331d5248d
SHIVAM RISHINARAYAN SINGH <109292385+SHIV000000@users.noreply.github.com>
2023-11-21 00:32:01 +0530

    Update settings.py

--- LearnMate/settings.py
+++ LearnMate/settings.py
@@ -130,10 +130,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    os.path.join(BASE_DIR, "staticfiles/")
 ]
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
