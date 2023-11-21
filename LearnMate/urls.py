5eea49b3a1b1a131724f177f0971eef50273a0d0
SHIVAM RISHINARAYAN SINGH <109292385+SHIV000000@users.noreply.github.com>
2023-11-21 21:51:14 +0530

    new database

--- LearnMate/urls.py
+++ LearnMate/urls.py
@@ -9,8 +9,3 @@ urlpatterns = [
     path('', include('base.urls')),
     path('api/', include('base.api.urls'))
 ]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-if settings.DEBUG:
- urlpatterns += path('admin/', admin.site.urls)
